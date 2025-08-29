c8b4bb3adfbee5cae8a089c4ddd98368cbd082a9
SO <sokogaiino@gmail.com>
2025-08-29 22:26:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -241,7 +241,12 @@ def digits_to_float(hund, tens, ones, tenths):
 # --------------------------------
 # 画面本体
 # --------------------------------
-st.title("📈 Weight-Trakcer")
+col1, col2 = st.columns([0.1, 0.9])
+with col1:
+    st.image("favicon.png", width=32)
+with col2:
+    st.title("Weight-Trakcer")
+
 
 # セッション初期化
 if "current_user" not in st.session_state: st.session_state.current_user = None
